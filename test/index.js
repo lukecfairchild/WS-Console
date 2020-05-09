9faef2465f7417d57351bdc2d251d418a0a7e7ac
Luke Fairchild <lukecfairchild@gmail.com>
2020-05-09 12:07:31 -0600

    more structural changes

--- test/index.js
+++ test/index.js
@@ -6,7 +6,8 @@ const WSConsole = require('../src');
 const server = new WSConsole.server({
 	webSocketPort : 9000,
 	webServerPort : 8081,
-	cacheSize     : 300
+	cacheSize     : 300,
+	dbPath        : Path.join(__dirname, 'db.json')
 	//ssl           : false,
 	//sslKey        : '/path/to/key',
 	//sslCert       : '/path/to/cert'
