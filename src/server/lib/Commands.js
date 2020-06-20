00f1e145089bea09ff8a78d22affbfbea6bc2246
Luke Fairchild <lukecfairchild@gmail.com>
2020-06-20 15:04:00 -0600

    refactoring

--- src/server/lib/Commands.js
+++ src/server/lib/Commands.js
@@ -3,31 +3,30 @@ const FileSystem = require('fs');
 const Path       = require('path');
 
 class Commands {
+	#commands = {};
 	constructor () {
 		const files = FileSystem.readdirSync(Path.join(__dirname, 'commands'));
 
 		files.forEach(fileName => {
-			const permission = fileName.split('.').slice(0, -1).join('.');
-			const filePath   = Path.join(__dirname, fileName);
+			const filePath = Path.join(__dirname, 'commands', fileName);
+			const command  = fileName.split('.').slice(0, -1).join(' ');
 
-			console.log(filePath, permission);
+			this.#commands[command] = require(filePath);
 		});
+	}
+
+	run (rawCommand) {
+		const command = rawCommand.trim();
+
+		for (const i in command.split(' ')) {
+			const commandParts = command.split(' ');
+			const check        = commandParts.splice(0, commandParts.length - i).join(' ');
+
+			if (this.#commands[check]) {
+
+			}
+		}
 	}
 }
 
 module.exports = Commands;
-
-
-
-const __dirname  = process.env.PWD;
-const FileSystem = require('fs');
-const Path       = require('path');
-
-const files = FileSystem.readdirSync(Path.join(__dirname, 'commands'));
-
-files.forEach(fileName => {
-	const permission = fileName.split('.').slice(0, -1).join('.');
-	const filePath   = Path.join(__dirname, 'commands', fileName);
-
-	console.log(filePath, permission);
-});
