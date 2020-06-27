edf06e316daea494692fe21b96e321e4727eeea2
Luke Fairchild <lukecfairchild@gmail.com>
2020-06-26 23:49:58 -0600

    refactoring

--- src/server/lib/Commands.js
+++ src/server/lib/Commands.js
@@ -4,7 +4,7 @@ const Path       = require('path');
 
 class Commands {
 	#commands = {};
-	#user;
+
 	constructor () {
 		const files = FileSystem.readdirSync(Path.join(__dirname, 'commands'));
 
@@ -12,23 +12,42 @@ class Commands {
 			const filePath = Path.join(__dirname, 'commands', fileName);
 			const command  = fileName.split('.').slice(0, -1).join(' ');
 
-			this.#commands[command] = require(filePath);
+			this.add(command, require(filePath));
 		});
 	}
 
-	run (commandRaw) {
+	add (command, commandPackage) {
+		if (this.#commands[command]) {
+			throw new Error(`Command already exists: ${command}`);
+		}
+
+		this.#commands[command] = commandPackage;
+	}
+
+	remove (command) {
+		delete this.#commands[command];
+	}
+
+	get (commandRaw) {
 		const commandTrimmed = commandRaw.trim();
 
 		for (const i in commandTrimmed.split(' ')) {
 			const commandParts = commandTrimmed.split(' ');
 			const command      = commandParts.splice(0, commandParts.length - i).join(' ');
 
-			if (this.#commands[command]
-			&&  (!this.#commands[command].permissions
-			||   this.#user.hasPermission(this.#commands[command].permissions))) {
-				this.#commands[command].handler(...commandParts);
+			if (this.#commands[command]) {
+				return {
+					handler : () => {
+						this.#commands[command].handler(...commandParts);
+					},
+					permissions : this.#commands[command].permissions
+				};
 			}
 		}
+	}
+
+	run (commandRaw) {
+		this.get(commandRaw).handler();
 	}
 }
 
