0d91cb3441063fde1f8c9a07c96e7a3c0e8fb706
Luke Fairchild <lukecfairchild@gmail.com>
2020-07-07 18:57:50 -0600

    refactoring

--- src/server/lib/Commands.js
+++ src/server/lib/Commands.js
@@ -17,10 +17,6 @@ class Commands {
 	}
 
 	add (command, commandPackage) {
-		if (this.#commands[command]) {
-			throw new Error(`Command already exists: ${command}`);
-		}
-
 		this.#commands[command] = commandPackage;
 	}
 
@@ -33,17 +29,28 @@ class Commands {
 
 		for (const i in commandTrimmed.split(' ')) {
 			const commandParts = commandTrimmed.split(' ');
-			const command      = commandParts.splice(0, commandParts.length - i).join(' ');
+			const commandName  = commandParts.splice(0, commandParts.length - i).join(' ');
 
-			if (this.#commands[command]) {
-				return {
-					handler : () => {
-						this.#commands[command].handler(...commandParts);
-					},
-					permissions : this.#commands[command].permissions
+			if (this.#commands[commandName]) {
+				const command = this.#commands[commandName];
+
+				const returns = async (...args) => {
+					return await command.handler(...args);
 				};
+
+				returns.handler     = command.handler;
+				returns.permissions = command.permissions;
+
+				return returns;
 			}
 		}
+
+		return {
+			handler : () => {
+				return 'Command not found.';
+			},
+			permissions : []
+		};
 	}
 
 	run (commandRaw) {
