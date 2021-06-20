14b48b26fb43da05ed653714a6aa290dbe1efabd
Luke Fairchild <lukecfairchild@gmail.com>
2021-06-20 03:15:30 -0600

    fix task logout description

---
+++ src/server/lib/commands/task.logout.js
@@ -0,0 +1,24 @@
+
+const Command = require('../Command');
+
+class TaskDelete extends Command {
+	constructor (options) {
+		super(options);
+
+		this.arguments   = '<task name>';
+		this.description = 'Logs out a task account';
+		this.permissions = [];
+
+		this.Tasks = this.Commands.Server.Accounts.Tasks;
+	}
+
+	async run (name) {
+		if (!this.Tasks.exists(name)) {
+			return `An Account does not exist with that name: "${name}"`;
+		}
+
+		this.Tasks.delete(name);
+	}
+}
+
+module.exports = TaskDelete;
