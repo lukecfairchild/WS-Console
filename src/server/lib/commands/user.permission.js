3f33a895db7a18d2a271ae0d1f960c033da1aeeb
Luke Fairchild <lukecfairchild@gmail.com>
2021-07-17 13:56:30 -0600

    almost done with permission commands

---
+++ src/server/lib/commands/user.permission.js
@@ -0,0 +1,39 @@
+
+const Command      = require('../Command');
+const IndentString = require('indent-string');
+
+class UserPermission extends Command {
+	constructor (options) {
+		super(options);
+
+		this.description = '';
+		this.permissions = [];
+	}
+
+	async run () {
+		const commands = this.Account.Commands.getAll();
+
+		const results = [];
+
+		for (let i in commands) {
+			const command = commands[i];
+			let result    = i;
+
+			if (result.match('^user permission')) {
+				if (command.arguments) {
+					result += ' ' + command.arguments;
+				}
+
+				if (command.description) {
+					result += '\n' + IndentString(command.description, 4);
+				}
+
+				results.push(result);
+			}
+		}
+
+		return results.join('\n');
+	}
+}
+
+module.exports = UserPermission;
