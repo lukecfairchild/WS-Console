7df3e297df491ae456c872a94cfcc987d4e3bd2a
Luke Fairchild <lukecfairchild@gmail.com>
2021-12-05 12:34:40 -0700

    webclient did not have target key in events

--- src/server/html/index.js
+++ src/server/html/index.js
@@ -279,7 +279,7 @@ document.addEventListener('keypress', (event) => {
 				webSocket.send(JSON.stringify({
 					type   : 'user',
 					action : 'command',
-					name   : name,
+					target : name,
 					data   : command
 				}));
 			}
